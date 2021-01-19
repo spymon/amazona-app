fd63d830c68b3b428cbadc9bb6bec624bcd4a69b
Simon Žižek <spymon12@gmail.com>
2021-01-19 23:04:26 +0100

    installed redux, react-redux and redux-thunk, added store, product action, reducer, product details action, reducer, add product constants.

---
+++ frontend/src/store.js
@@ -0,0 +1,24 @@
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux'
+import thunk from 'redux-thunk'
+import {
+  productDetailsReducer,
+  productListReducer,
+} from './reducers/productReducers'
+
+const initialState = {}
+
+const reducer = combineReducers({
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+})
+
+//this activate redux DevTools
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeEnhancer(applyMiddleware(thunk))
+)
+
+export default store
