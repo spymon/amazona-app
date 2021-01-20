2d21e06a9e8f0562beb66c6b43a7567fb78d35dd
Simon Žižek <spymon12@gmail.com>
2021-01-20 21:32:13 +0100

    changed ancor to Links, added new reducer, action, contant to cart, saved cart co localstorage, use addToCart action in CartScreen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,15 +1,23 @@
 import { applyMiddleware, combineReducers, compose, createStore } from 'redux'
 import thunk from 'redux-thunk'
+import { cartReducers } from './reducers/cartReducers'
 import {
   productDetailsReducer,
   productListReducer,
 } from './reducers/productReducers'
 
-const initialState = {}
+const initialState = {
+  cart: {
+    cartItems: localStorage.getItem('cartItems')
+      ? JSON.parse(localStorage.getItem('cartItems'))
+      : [],
+  },
+}
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducers,
 })
 
 //this activate redux DevTools
