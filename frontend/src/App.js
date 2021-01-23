91e7a4b8f30657b6a413010a665c35fa4dbd504b
Simon Žižek <spymon12@gmail.com>
2021-01-23 19:39:47 +0100

    add SigninScreen component, some css styles and use is as a Route in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import ProductScreen from './screens/ProductScreen'
 import HomeScreen from './screens/HomeScreen'
 import CartScreen from './screens/CartScreen'
 import { useSelector } from 'react-redux'
+import SigninScreen from './screens/SigninScreen'
 
 function App() {
   const cart = useSelector(state => state.cart)
@@ -29,6 +30,7 @@ function App() {
         <main>
           <Route path="/cart/:id?" component={CartScreen}></Route>
           <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/signin" component={SigninScreen}></Route>
           <Route path="/" component={HomeScreen} exact></Route>
         </main>
         <footer className="row center">All right reserved</footer>
