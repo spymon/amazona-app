ad1814f623792edd73e25ad392f05695bf2c393c
Simon Žižek <spymon12@gmail.com>
2021-01-23 14:01:21 +0100

    add productModel, productRout, impliment seeding from data to the database, api route for all products and api route for a single product by id

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import mongoose from 'mongoose'
-import data from './data.js'
+import productRouter from './routers/productRouter.js'
 import userRouter from './routers/userRouter.js'
 
 const app = express()
@@ -15,20 +15,8 @@ mongoose.connect(
   () => console.log('Connected to the DB')
 )
 
-app.get('/api/products/:id', (req, res) => {
-  const product = data.products.find(product => product._id === req.params.id)
-  if (product) {
-    res.send(product)
-  } else {
-    res.status(404).send({ message: 'Product not Found' })
-  }
-})
-
-app.get('/api/products', (req, res) => {
-  res.send(data.products)
-})
-
 app.use('/api/users', userRouter)
+app.use('/api/products', productRouter)
 
 app.get('/', (req, res) => res.send('server is ready'))
 
